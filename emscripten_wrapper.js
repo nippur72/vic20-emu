d3c95669c2c27eb8b01fb354b96374e727dda60d
nino-porcino <nino.porcino@gmail.com>
2023-08-12 09:49:14 +0200

    upgrade to emcc 3.1.31

--- emscripten_wrapper.js
+++ emscripten_wrapper.js
@@ -18,73 +18,69 @@ let keyboard_poll;
 
 let vic20 = { tape: { } };
 
-function load_wasm(ready_cb) {
-
+async function load_wasm() {
    // emscripten_module.js exports "emscripten_module" globally
 
-   let instance = emscripten_module({ wasmBinary: emscripten_wasm_binary, onRuntimeInitialized: ()=>{
-      // makes C exported functions available globally
+   let instance = await emscripten_module();
 
-      vic20.init     = instance.cwrap("sys_init", null);
-      vic20.config   = instance.cwrap("sys_config", null, ['number']);
-      vic20.exec     = instance.cwrap("sys_exec", null);
-      vic20.exec_us  = instance.cwrap("sys_exec_us", ['number']);
-      vic20.reset    = instance.cwrap("sys_reset", null);
+   // makes C exported functions available globally
 
-      vic20.key_down = instance.cwrap("sys_key_down" , null, ['number'] );
-      vic20.key_up   = instance.cwrap("sys_key_up"   , null, ['number'] );
-      vic20.joystick = instance.cwrap("sys_joystick" , null, ['number'] );
+   vic20.init     = instance.cwrap("sys_init", null);
+   vic20.config   = instance.cwrap("sys_config", null, ['number']);
+   vic20.exec     = instance.cwrap("sys_exec", null);
+   vic20.exec_us  = instance.cwrap("sys_exec_us", ['number']);
+   vic20.reset    = instance.cwrap("sys_reset", null);
 
-      vic20.emu_joy  = instance.cwrap("sys_set_joystick_type" , null, ['number'] );
+   vic20.key_down = instance.cwrap("sys_key_down" , null, ['number'] );
+   vic20.key_up   = instance.cwrap("sys_key_up"   , null, ['number'] );
+   vic20.joystick = instance.cwrap("sys_joystick" , null, ['number'] );
 
-      vic20.load_prg         = instance.cwrap("sys_quick_load" , null, ['array', 'number'] );
-      vic20.insert_cartdrige = instance.cwrap("sys_insert_rom_cartdrige" , null, ['array', 'number'] );
+   vic20.emu_joy  = instance.cwrap("sys_set_joystick_type" , null, ['number'] );
 
-      vic20.peek        = instance.cwrap("sys_mem_cpu_rd"   , 'number', ['number'] );
-      vic20.poke        = instance.cwrap("sys_mem_cpu_wr"   , null, ['number', 'number'] );
+   vic20.load_prg         = instance.cwrap("sys_quick_load" , null, ['array', 'number'] );
+   vic20.insert_cartdrige = instance.cwrap("sys_insert_rom_cartdrige" , null, ['array', 'number'] );
 
-      vic20.dump_vic = instance.cwrap("sys_dump_vic"   , null );
+   vic20.peek        = instance.cwrap("sys_mem_cpu_rd"   , 'number', ['number'] );
+   vic20.poke        = instance.cwrap("sys_mem_cpu_wr"   , null, ['number', 'number'] );
 
-      vic20.cas_port = instance.cwrap("sys_cas_port"   , 'number');
+   vic20.dump_vic = instance.cwrap("sys_dump_vic"   , null );
 
-      vic20.tape.insert = instance.cwrap("sys_insert_tape"  , 'bool', ['array', 'number'] );
-      vic20.tape.remove = instance.cwrap("sys_remove_tape"  , null );
-      vic20.tape.play   = instance.cwrap("sys_tape_play"    , null );
-      vic20.tape.stop   = instance.cwrap("sys_tape_stop"    , null );
+   vic20.cas_port = instance.cwrap("sys_cas_port"   , 'number');
 
-      mem_read = vic20.peek;
-      mem_write = vic20.poke;
+   vic20.tape.insert = instance.cwrap("sys_insert_tape"  , 'bool', ['array', 'number'] );
+   vic20.tape.remove = instance.cwrap("sys_remove_tape"  , null );
+   vic20.tape.play   = instance.cwrap("sys_tape_play"    , null );
+   vic20.tape.stop   = instance.cwrap("sys_tape_stop"    , null );
 
-      /*
-      cpu_init            = instance.cwrap("cpu_init", null);
-      cpu_reset           = instance.cwrap("cpu_reset", null);
-      cpu_run_instruction = instance.cwrap("cpu_run_instruction", 'number');
+   mem_read = vic20.peek;
+   mem_write = vic20.poke;
 
-      mem_read           = instance.cwrap("mem_read", 'number', ['number']);
-      mem_write          = instance.cwrap("mem_write", null, ['number', 'number']);
-      rom_load           = instance.cwrap("rom_load", null, ['number', 'number']);
+   /*
+   cpu_init            = instance.cwrap("cpu_init", null);
+   cpu_reset           = instance.cwrap("cpu_reset", null);
+   cpu_run_instruction = instance.cwrap("cpu_run_instruction", 'number');
 
-      io_read            = instance.cwrap("io_read", 'number', ['number']);
-      io_write           = instance.cwrap("io_write", null, ['number', 'number']);
+   mem_read           = instance.cwrap("mem_read", 'number', ['number']);
+   mem_write          = instance.cwrap("mem_write", null, ['number', 'number']);
+   rom_load           = instance.cwrap("rom_load", null, ['number', 'number']);
 
-      lm80c_tick         = instance.cwrap("lm80c_tick", 'number');
-      lm80c_set_debug    = instance.cwrap("lm80c_set_debug", null, ['bool']);
-      lm80c_ctc_enable   = instance.cwrap("lm80c_ctc_enable", null, ['bool']);
-      lm80c_init         = instance.cwrap("lm80c_init", null);
-      lm80c_reset        = instance.cwrap("lm80c_reset", null);
+   io_read            = instance.cwrap("io_read", 'number', ['number']);
+   io_write           = instance.cwrap("io_write", null, ['number', 'number']);
 
-      lm80c_tick_line    = instance.cwrap("lm80c_tick_line", 'number', ['number']);
+   lm80c_tick         = instance.cwrap("lm80c_tick", 'number');
+   lm80c_set_debug    = instance.cwrap("lm80c_set_debug", null, ['bool']);
+   lm80c_ctc_enable   = instance.cwrap("lm80c_ctc_enable", null, ['bool']);
+   lm80c_init         = instance.cwrap("lm80c_init", null);
+   lm80c_reset        = instance.cwrap("lm80c_reset", null);
 
-      keyboard_reset     = instance.cwrap("keyboard_reset"  , null );
-      keyboard_press     = instance.cwrap("keyboard_press"  , null, ['number', 'number'] );
-      keyboard_release   = instance.cwrap("keyboard_release", null, ['number', 'number'] );
-      keyboard_poll      = instance.cwrap("keyboard_poll"   , 'number', ['number'] );
-      */
+   lm80c_tick_line    = instance.cwrap("lm80c_tick_line", 'number', ['number']);
 
-      // export instance globally (not strictly required)
-      wasm_instance = instance;
+   keyboard_reset     = instance.cwrap("keyboard_reset"  , null );
+   keyboard_press     = instance.cwrap("keyboard_press"  , null, ['number', 'number'] );
+   keyboard_release   = instance.cwrap("keyboard_release", null, ['number', 'number'] );
+   keyboard_poll      = instance.cwrap("keyboard_poll"   , 'number', ['number'] );
+   */
 
-      // finished
-      ready_cb();
-   }});
+   // export instance globally (not strictly required)
+   wasm_instance = instance;
 }
--- mkwasm.bat
+++ mkwasm.bat
@@ -1,18 +1,17 @@
 @echo off
 
-rem abilitare il path con
+rem abilitare il path a emcc 3.1.31 con:
 rem ..\emsdk-master\emsdk_env.bat
 
 call emcc wasm\prova.c -O3 ^
-   -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" ^
+   -s EXPORTED_RUNTIME_METHODS=ccall,cwrap ^
    -s ENVIRONMENT=web ^
    -s MODULARIZE=1 ^
    -s EXPORT_NAME="emscripten_module" ^
+   -s ALLOW_MEMORY_GROWTH=1 ^
    -o emscripten_module.js
     
 echo emcc finished, putting wasm binary into UInt8Array
-
-node wasm\make_array > emscripten_module.wasm.array.js
 
 echo done
 
